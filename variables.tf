b30c65b94deff0c6408305ccdfc2e80c42dfa8b1
Jason Thompson <CloudNetworkArchitect.JT@gmail.com>
2024-05-11 16:54:10 -0700

    Fix syntax error in variables.tf

--- variables.tf
+++ variables.tf
@@ -3,25 +3,21 @@ variable "vpc_id" {
   description = "You'll need to have a VPC ready to go. Provide the VPC ID here"
 }
 
-
 variable "my_ip" {
   type        = string
   description = "Provide your IPv4 address here. This is the intended format,<Your OWN IP>/32"
 }
-
 
 variable "public_key" {
   type        = string
   description = "Create a SSH Keypair and the paste in the public key here"
 }
 
-
 variable "instance_type" {
   type        = string
   default     = "t3.micro"
   description = "Enter your EC2 Instance Type. It defaults to t3.micro"
 }
-
 
 variable "server_name" {
   type        = string
