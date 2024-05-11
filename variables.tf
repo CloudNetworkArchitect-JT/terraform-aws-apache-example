fb129679cacac2bc09f692271ab9b49fe60a1530
Jason Thompson <CloudNetworkArchitect.JT@gmail.com>
2024-05-11 16:43:28 -0700

    Attempting to resolve Missing newline after argument: An argument definition must end with a newline. (and 3 other messages) (in variables.tf on line 8) Error in Terraform Registry

--- variables.tf
+++ variables.tf
@@ -3,15 +3,18 @@ variable "vpc_id" {
   description = "You'll need to have a VPC ready to go. Provide the VPC ID here"
 }
 
+
 variable "my_ip" {
   type        = string
   description = "Provide your IPv4 address here. This is the intended format,<Your OWN IP>/32"
 }
 
+
 variable "public_key" {
   type        = string
   description = "Create a SSH Keypair and the paste in the public key here"
 }
+
 
 variable "instance_type" {
   type        = string
@@ -19,11 +22,13 @@ variable "instance_type" {
   description = "Enter your EC2 Instance Type. It defaults to t3.micro"
 }
 
+
 variable "server_name" {
   type        = string
   default     = "Apache Example Server"
   description = "Provide a server name. It defaults to Apache Example Server."
 }
+
 
 # Input Variables
 
